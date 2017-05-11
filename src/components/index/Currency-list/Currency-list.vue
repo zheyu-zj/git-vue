26dc7e4561c984d6c34b4da7165fee51b2b2521d
zj <zj@gmail.com>
2017-05-11 10:49:13 +0800

    修改webpack config API代理

---
+++ src/components/index/Currency-list/Currency-list.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="18">
+                <el-table class="table" :data="tableDatas" style="width: 100%" :default-sort="{prop: 'date',order: 'descending'}">
+                    <el-table-column label="币名">
+                        <template scope="scope">
+                            <img :src="scope.row.url" class="tradeList-icon">
+                            <span style="margin-left: 10px">{{ scope.row.name }}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="newPrice" label="最新价格(CNY)" sortable>
+                    </el-table-column>
+                    <el-table-column prop="volume24" label="24H成交量" sortable>
+                    </el-table-column>
+                    <el-table-column prop="turnover24" label="24H成交额(CNY)" sortable>
+                    </el-table-column>
+                    <el-table-column prop="range24" label="24H涨跌" sortable>
+                    </el-table-column>
+                    <el-table-column prop="range7" label="7D涨跌" sortable>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+            <el-col :span="6">
+                <a href="javascript:">
+                </a>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import axios from 'axios'
+    Vue.prototype.$http = axios;
+
+    const ERR_OK = 200;
+    export default {
+        data () {
+            return {
+                tableDatas: ''
+            }
+        },
+        created: function () {
+            this.$http.get('/json/index.json').then((response) => {
+                if (response.data === ERR_OK) {
+                    this.tableDatas = response.data.index;
+                }
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -9,24 +9,24 @@
             <a class="new-announcement" href="javascript:"><i class="el-icon-edit"></i>公告：关于文创币官方申请文创币取消每日10%涨跌幅限制公告</a>
             <el-row>
                 <el-col :span="18">
-                    <el-table class="table" :data="tableDatas" style="width: 100%" :default-sort="{prop: 'date',order: 'descending'}">
-                        <el-table-column label="币名">
-                            <template scope="scope">
-                                <img :src="scope.row.url" class="tradeList-icon">
-                                <span style="margin-left: 10px">{{ scope.row.name }}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="newPrice" label="最新价格(CNY)" sortable>
-                        </el-table-column>
-                        <el-table-column prop="volume24" label="24H成交量" sortable>
-                        </el-table-column>
-                        <el-table-column prop="turnover24" label="24H成交额(CNY)" sortable>
-                        </el-table-column>
-                        <el-table-column prop="range24" label="24H涨跌" sortable>
-                        </el-table-column>
-                        <el-table-column prop="range7" label="7D涨跌" sortable>
-                        </el-table-column>
-                    </el-table>
+                    <!--<el-table class="table" :data="tableDatas" style="width: 100%" :default-sort="{'prop':'date','order':'descending'}">-->
+                        <!--<el-table-column label="币名">-->
+                            <!--<template scope="scope">-->
+                                <!--<img :src="scope.row.url" class="tradeList-icon">-->
+                                <!--<span style="margin-left: 10px">{{ scope.row.name }}</span>-->
+                            <!--</template>-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column prop="newPrice" label="最新价格(CNY)" sortable>-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column prop="volume24" label="24H成交量" sortable>-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column prop="turnover24" label="24H成交额(CNY)" sortable>-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column prop="range24" label="24H涨跌" sortable>-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column prop="range7" label="7D涨跌" sortable>-->
+                        <!--</el-table-column>-->
+                    <!--</el-table>-->
                 </el-col>
                 <el-col :span="6">
                     <a href="javascript:">
@@ -37,29 +37,24 @@
     </div>
 </template>
 <script>
-    import Vue from 'vue'
-    import axios from 'axios'
-    axios.defaults.baseURL = 'http://localhost:3000';
-    Vue.prototype.$http = axios;
-    const ERR_OK = 0;
+//    import Vue from 'vue'
+//    import axios from 'axios'
+//    Vue.prototype.$http = axios;
+//    const ERR_OK = 200;
     export default {
-        data () {
-            return {
-                tableDatas: ''
-            }
-        },
-        created: function () {
-            this.$http.get('/json/index.json',{withCredentials:true}).then((response) => {
-                console.log(response);
-                console.log(response.index)
-//                response = response.data;
-//                if (response.errno === ERR_OK) {
-//                    this.tableDatas = response.data;
-//                    console.log(this.tableDatas)
+//        data () {
+//            return {
+//                tableDatas: ''
+//            }
+//        },
+//        created: function () {
+//            this.$http.get('/json/index.json').then((response) => {
+//                if (response.data === ERR_OK) {
+//                    this.tableDatas = response.data.index;
 //                }
-            })
-        }
-    }
+//            })
+//        }
+    }   
 </script>
 
 <style lang="scss">
